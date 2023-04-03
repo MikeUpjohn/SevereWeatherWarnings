20d7db21b14215872164becb432097c2184f2f58
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2023-04-03 19:59:00 +0100

    13 Added warnings core

--- SevereWeatherWarnings.Map/wwwroot/js/warnings.js
+++ SevereWeatherWarnings.Map/wwwroot/js/warnings.js
@@ -2,18 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-let map;
+//let map;
 
 $(document).ready(function () {
     var mapToken = $('#map').data('accesstoken');
     mapboxgl.accessToken = mapToken;
 
-    map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mikeupjohn/cleyos0o0000t01p9r4ovd33n',
-        center: [- 97.043, 32.896],
-        zoom: 9
-    });
+    createMap(32.896, -97.043, 9);
 });
 
 $('#flash-flood-warnings').click(function () {
@@ -28,40 +23,12 @@ $('#flash-flood-warnings').click(function () {
 
 function addWarningsToMap(data) {
     $(data.weatherWarnings).each(function (index, item) {
-        console.log(item);
-        map.addSource(item.id, {
-            'type': 'geojson',
-            'data': {
-                'type': 'Feature',
-                'geometry': {
-                    'type': 'Polygon',
-                    'coordinates': item.warningGeometry.coOrdinates
-                }
-            }
-        });
-
-        map.addLayer({
-            'id': item.id,
-            'type': 'fill',
-            'source': item.id,
-            'layout': {},
-            'paint': {
-                'fill-color': '#00FF00',
-                'fill-opacity': 0.15
-            }
-        });
-
-        map.addLayer({
-            'id': 'outline' + item.id,
-            'type': 'line',
-            'source': item.id,
-            'layout': {},
-            'paint': {
-                'line-color': '#00FF00',
-                'line-width': 2
-            }
-        });
-
-        console.log('Added layer with id ' + item.id + ' to the map');
+        drawWarningPolygon(item);
     });
+}
+
+function drawWarningPolygon(dataItem) {
+    addDataSourceToMap(dataItem.id, dataItem.warningGeometry.coOrdinates);
+    drawPolygon(dataItem.id, '#00FF00', 0.15);
+    drawPolygonBorder(dataItem.id, '#00FF00', 0.15);
 }
