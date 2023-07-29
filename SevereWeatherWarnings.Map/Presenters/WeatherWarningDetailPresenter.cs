7b5423a5144cc09509aac0371b6fef310d77b36f
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2023-07-29 07:27:55 +0100

    51 Populating view model.

---
+++ SevereWeatherWarnings.Map/Presenters/WeatherWarningDetailPresenter.cs
@@ -0,0 +1,27 @@
+﻿using SevereWeatherWarnings.Map.Presenters.Interfaces;
+using SevereWeatherWarnings.Models.Display;
+using SevereWeatherWarnings.Models.Display.Common;
+using APIWeatherWarning = SevereWeatherWarnings.Models.API.WeatherWarning;
+
+namespace SevereWeatherWarnings.Map.Presenters
+{
+    public class MapWarningDetailPresenter : IMapWarningDetailPresenter
+    {
+        public WeatherWarningDetailsViewModel Present(APIWeatherWarning weatherWarning)
+        {
+            WeatherWarningDetailsViewModel viewModel = new WeatherWarningDetailsViewModel
+            {
+                Id = weatherWarning.Id,
+                Type = weatherWarning.Type,
+                WarningGeometry = MapWarningGeometry(weatherWarning)
+            };
+
+            return viewModel;
+        }
+
+        private Geometry MapWarningGeometry(APIWeatherWarning weatherWarning)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
