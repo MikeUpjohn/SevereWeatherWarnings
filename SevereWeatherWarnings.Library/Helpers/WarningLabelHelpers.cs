5145ed0d697048c39b7e2ecaa322db6e03c70f66
Mike Upjohn <michael.upjohn@hotmail.co.uk>
2023-10-05 22:23:40 +0100

    77 Refactor Tag Buildler

--- SevereWeatherWarnings.Library/Helpers/WarningLabelHelpers.cs
+++ SevereWeatherWarnings.Library/Helpers/WarningLabelHelpers.cs
@@ -10,74 +10,21 @@ namespace SevereWeatherWarnings.Library.Helpers
     {
         public static IHtmlContent WindThreatLabel(this IHtmlHelper htmlHelper, WindThreatParameter windThreatParameter)
         {
-            var label = new TagBuilder("span");
-            label.MergeAttribute("class", "standard-label");
-            label.InnerHtml.SetContent(EnumExtensions.GetDescription(windThreatParameter.ParameterType));
-
-            var value = new TagBuilder("span");
-            value.MergeAttribute("class", $"standard-label-value {windThreatParameter.CssClass}");
-            value.InnerHtml.SetContent(EnumExtensions.GetDescription(windThreatParameter.DisplayValue));
-
-            var container = new TagBuilder("span");
-            container.MergeAttribute("class", "container-label");
-            container.InnerHtml.AppendHtml(label.GetString());
-            container.InnerHtml.AppendHtml(value.GetString());
-
-            return new HtmlString(container.GetString());
+            return SevereWeatherLabel(htmlHelper, windThreatParameter.ParameterType, windThreatParameter.DisplayValue, windThreatParameter.CssClass);
         }
 
         public static IHtmlContent MaxWindGustLabel(this IHtmlHelper htmlHelper, MaxWindGustParameter maxWindGustParameter)
         {
-            var label = new TagBuilder("span");
-            label.MergeAttribute("class", "standard-label");
-            label.InnerHtml.SetContent(EnumExtensions.GetDescription(maxWindGustParameter.ParameterType));
-
-            var value = new TagBuilder("span");
-            value.MergeAttribute("class", $"standard-label-value {maxWindGustParameter.CssClass}");
-            value.InnerHtml.SetContent(maxWindGustParameter.DisplayValue);
-
-            var container = new TagBuilder("span");
-            container.MergeAttribute("class", "container-label");
-            container.InnerHtml.AppendHtml(label.GetString());
-            container.InnerHtml.AppendHtml(value.GetString());
-
-            return new HtmlString(container.GetString());
+            return SevereWeatherLabel(htmlHelper, maxWindGustParameter.ParameterType, maxWindGustParameter.DisplayValue, maxWindGustParameter.CssClass);
         }
 
         public static IHtmlContent HailThreatLabel(this IHtmlHelper htmlHelper, HailThreatParameter hailThreatParameter)
         {
-            var label = new TagBuilder("span");
-            label.MergeAttribute("class", "standard-label");
-            label.InnerHtml.SetContent(EnumExtensions.GetDescription(hailThreatParameter.ParameterType));
-
-            var value = new TagBuilder("span");
-            value.MergeAttribute("class", $"standard-label-value {hailThreatParameter.CssClass}");
-            value.InnerHtml.SetContent(EnumExtensions.GetDescription(hailThreatParameter.DisplayValue));
-
-            var container = new TagBuilder("span");
-            container.MergeAttribute("class", "container-label");
-            container.InnerHtml.AppendHtml(label.GetString());
-            container.InnerHtml.AppendHtml(value.GetString());
-
-            return new HtmlString(container.GetString());
+            return SevereWeatherLabel(htmlHelper, hailThreatParameter.ParameterType, hailThreatParameter.DisplayValue, hailThreatParameter.CssClass);
         }
 
         public static IHtmlContent ThunderstormDamageThreatLabel(this IHtmlHelper htmlHelper, ThunderstormDamageParameter thunderstormDamageParameter)
         {
-            //var label = new TagBuilder("span");
-            //label.MergeAttribute("class", "standard-label");
-            //label.InnerHtml.SetContent(EnumExtensions.GetDescription(thunderstormDamageParameter.ParameterType));
-
-            //var value = new TagBuilder("span");
-            //value.MergeAttribute("class", $"standard-label-value {thunderstormDamageParameter.CssClass}");
-            //value.InnerHtml.SetContent(EnumExtensions.GetDescription(thunderstormDamageParameter.DisplayValue));
-
-            //var container = new TagBuilder("span");
-            //container.MergeAttribute("class", "container-label");
-            //container.InnerHtml.AppendHtml(label.GetString());
-            //container.InnerHtml.AppendHtml(value.GetString());
-
-            //return new HtmlString(container.GetString());
             return SevereWeatherLabel(htmlHelper, thunderstormDamageParameter.ParameterType, thunderstormDamageParameter.DisplayValue, thunderstormDamageParameter.CssClass);
         }
 
@@ -90,6 +37,24 @@ namespace SevereWeatherWarnings.Library.Helpers
             var value = new TagBuilder("span");
             value.MergeAttribute("class", $"standard-label-value {cssClass}");
             value.InnerHtml.SetContent(EnumExtensions.GetDescription(displayValue));
+
+            var container = new TagBuilder("span");
+            container.MergeAttribute("class", "container-label");
+            container.InnerHtml.AppendHtml(label.GetString());
+            container.InnerHtml.AppendHtml(value.GetString());
+
+            return new HtmlString(container.GetString());
+        }
+
+        private static IHtmlContent SevereWeatherLabel(this IHtmlHelper htmlHelper, WarningParameterType warningParameterType, string displayValue, string cssClass)
+        {
+            var label = new TagBuilder("span");
+            label.MergeAttribute("class", "standard-label");
+            label.InnerHtml.SetContent(EnumExtensions.GetDescription(warningParameterType));
+
+            var value = new TagBuilder("span");
+            value.MergeAttribute("class", $"standard-label-value {cssClass}");
+            value.InnerHtml.SetContent(displayValue);
 
             var container = new TagBuilder("span");
             container.MergeAttribute("class", "container-label");
